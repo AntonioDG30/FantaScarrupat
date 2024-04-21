231a63bb0414ebac1db549159cd3921d1e87329a
AntonioDG30 <a.digiorgio8@studenti.unisa.it>
2024-04-21 18:45:24 +0200

    Bug Fix e inizio lavori partite

--- php/insertPartite.php
+++ php/insertPartite.php
@@ -1,47 +1,82 @@
 <?php
-// Includi la libreria PHPExcel
-require 'PHPExcel/Classes/PHPExcel.php';
+  // Importa connessione con il database
+  global $conn;
+  include 'connectionDB.php';
 
-// Carica il file Excel
-$objPHPExcel = PHPExcel_IOFactory::load('./file/Calendario_Serie-A.xlsx');
+  // Includi la libreria PhpSpreadsheet
+  require '../vendor/autoload.php';
 
-// Seleziona il foglio di lavoro (presumo che il foglio di lavoro sia il primo, puoi modificare di conseguenza se necessario)
-$sheet = $objPHPExcel->getActiveSheet();
+  use PhpOffice\PhpSpreadsheet\IOFactory;
 
-// Inizializza una stringa per memorizzare la query SQL
-$sql = '';
 
-// Itera sulle righe del foglio di lavoro
-foreach ($sheet->getRowIterator() as $row) {
-  // Ottieni i valori delle celle nella riga corrente
-  $cellIterator = $row->getCellIterator();
-  $cellIterator->setIterateOnlyExistingCells(false); // Considera tutte le celle, anche se vuote
+  // Query per recuperare i nomi delle squadre dalla tabella fantasquadre
+  $query = "SELECT nome_fantasquadra FROM fantasquadra";
 
-  // Inizializza un array per memorizzare i dati della partita
-  $data = array();
+  // Esegui la query
+  $result = $conn->query($query);
 
-  // Itera sulle celle della riga corrente
-  foreach ($cellIterator as $cell) {
-    // Aggiungi il valore della cella all'array dei dati
-    $data[] = $cell->getValue();
+  // Inizializza un array per memorizzare i nomi delle squadre
+  $nomiSquadre = array();
+
+  // Verifica se sono presenti risultati
+  if ($result->num_rows > 0) {
+    // Itera sui risultati e aggiungi i nomi delle squadre all'array
+    while ($row = $result->fetch_assoc()) {
+      $nomiSquadre[] = $row['nome_fantasquadra'];
+    }
   }
 
-  // Estrai i dati relativi alla partita
-  $fantasquadra_casa = $data[0];
-  $gol_casa = (int)$data[1];
-  $punti_casa = (float)$data[2];
-  $fantasquadra_trasferta = $data[3];
-  $gol_trasferta = (int)$data[4];
-  $punti_trasferta = (float)$data[5];
-  $giornata = (int)$data[6];
+  // Carica il file Excel
+  $objPHPExcel = IOFactory::load('../file/Calendario_Serie-A.xlsx');
 
-  // Prepara la query SQL per l'inserimento dei dati e aggiungila alla stringa SQL
-  $sql .= "INSERT INTO partita_avvessario (nome_fantasquadra_casa, gol_casa, punti_casa, nome_fantasquadra_trasferta, gol_trasferta, punti_trasferta, giornata) VALUES ('$fantasquadra_casa', $gol_casa, $punti_casa, '$fantasquadra_trasferta', $gol_trasferta, $punti_trasferta, $giornata);\n";
-}
+  // Seleziona il foglio di lavoro (presumo che il foglio di lavoro sia il primo, puoi modificare di conseguenza se necessario)
+  $sheet = $objPHPExcel->getActiveSheet();
 
-// Scrivi la stringa SQL in un file .sql
-$file = 'query_insert_partite.sql';
-file_put_contents($file, $sql);
+// Inizializza una stringa per memorizzare la query SQL
+  $sql = '';
 
-echo "Query SQL salvata correttamente nel file $file";
+  // Itera sulle righe del foglio di lavoro
+  foreach ($sheet->getRowIterator() as $indice => $row) {
+    // Ottieni i valori delle celle nella riga corrente
+    $cellIterator = $row->getCellIterator();
+    $cellIterator->setIterateOnlyExistingCells(false); // Considera tutte le celle, anche se vuote
+
+    // Inizializza un array per memorizzare i dati della partita
+    $data = array();
+
+    // Itera sulle celle della riga corrente
+    foreach ($cellIterator as $cell) {
+      // Aggiungi il valore della cella all'array dei dati
+      $data[] = $cell->getValue();
+    }
+
+
+    if (in_array($data[0], $nomiSquadre)) {
+      // Estrai i dati relativi alla partita
+      $fantasquadra_casa = $data[0];
+      echo $fantasquadra_casa, "\n";
+      $gol_casa = (int)$data[1];
+      echo $gol_casa, "\n";
+      $punti_casa = (float)$data[2];
+      echo $punti_casa, "\n";
+      $fantasquadra_trasferta = $data[3];
+      echo $fantasquadra_trasferta, "\n";
+      $gol_trasferta = $data[4];
+      echo $gol_trasferta, "\n";
+      $PROVA = $data[6];
+      echo $PROVA, "\n";
+    } else {
+      continue;
+    }
+
+    // Prepara la query SQL per l'inserimento dei dati e aggiungila alla stringa SQL
+    //$sql .= "INSERT INTO partita_avvessario (nome_fantasquadra_casa, gol_casa, punti_casa, nome_fantasquadra_trasferta, gol_trasferta, punti_trasferta, giornata) VALUES ('$fantasquadra_casa', $gol_casa, $punti_casa, '$fantasquadra_trasferta', $gol_trasferta, $punti_trasferta, $giornata);\n";
+  }
+
+  // Scrivi la stringa SQL in un file .sql
+  //$file = 'query_insert_partite.sql';
+  //file_put_contents($file, $sql);
+
+  //echo "Query SQL salvata correttamente nel file $file";
 ?>
+
