ae830dbe3020c789d112fc21089e3438d3ae3208
AntonioDG30 <a.digiorgio8@studenti.unisa.it>
2024-05-10 14:26:38 +0200

    BugFix generali, continuo lavori admin.php

---
+++ php/logout.php
@@ -0,0 +1,14 @@
+<?php
+// Inizia o ripristina la sessione
+session_start();
+
+// Distruggi tutte le variabili di sessione
+session_unset();
+
+// Distruggi la sessione
+session_destroy();
+
+// Reindirizza alla pagina index.php
+header("Location: ../index.php");
+exit; // Assicura che lo script termini qui
+?>
